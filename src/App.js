5b23e17d71d14da801c2412fdb39ff816d114ee8
ethan <ethanowicks9@gmail.com>
2024-02-10 11:57:58 +0000

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Footer from './components/footer';
 // import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 
@@ -9,6 +10,7 @@ function App() {
     <div className='app'>
       <Navbar />
       <Home />
+      <Footer />
 
     </div>
     
---
+++ src/components/footer.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import { FaPhone } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+
+
+
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <div className="row">
+            <div className="col-4 d-flex justify-content-center align-items-center flex-column my-5">
+                <div className="logo">
+                    <p>logo</p>
+                </div>
+                <div className="phone mt-3">
+                    <FaPhone />
+                    <p className='phone-number'>0203 930 3139</p>
+                </div>
+                <div className="phone">
+                    <FaPhone />
+                    <p className='phone-number'>0203 930 3139</p>
+                </div>
+                <div className="email">
+                    <MdEmail />
+                    <a href="#">ethanowicks9@gmail.com</a>
+                </div>
+                <p className='my-4'>© 2024 ShapeHouse | FAQ Privacy Policy</p>
+            </div>
+            <div className="col-8 d-flex justify-content-center align-items-center">
+                <div className="footer-column">
+                    <ul>
+                        <p>contact</p>
+                        <li>test</li>
+                        <li>test</li>
+                        <li>test</li>
+                        <li>test</li>
+                    </ul>
+                </div>
+                <div className="footer-column">
+                    <ul>
+                        <p>contact</p>
+                        <li>test</li>
+                        <li>test</li>
+                        <li>test</li>
+                        <li>test</li>
+                    </ul>
+                </div>
+                <div className="footer-column">
+                    <ul>
+                        <p>contact</p>
+                        <li>test</li>
+                        <li>test</li>
+                        <li>test</li>
+                        <li>test</li>
+                    </ul>
+                </div>
+                <div className="footer-column">
+                    <ul>
+                        <p>contact</p>
+                        <li>test</li>
+                        <li>test</li>
+                        <li>test</li>
+                        <li>test</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+           
+    </div>
+  )
+}
+
+export default Footer
