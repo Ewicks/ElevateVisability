c5cf1d57a9074f16f4fb2b10b34df57d1ca90bca
ethan <ethanowicks9@gmail.com>
2024-02-10 13:20:51 +0000

    Add pricing page

--- src/pages/pricing.jsx
+++ src/pages/pricing.jsx
@@ -2,7 +2,98 @@ import React from 'react'
 
 function pricing() {
   return (
-    <div>pricing</div>
+    <div>
+      <div className="container">
+        <h1 className='text-light text-center mt-5 pricing-title'>Pricing</h1>
+        <p className='text-center pricing-subtitle mb-5'>Plans that empower you and your team to ship without friction.</p>
+        <div className="row mb-5">
+          <div className="col-md-4 col-sm-12">
+            <div className="card blue-card">
+              <h2 className='card-type card-type-blue'>Enterprise</h2>
+              <p className='card-type-text blue-text'>For larger enterprises with support SLA and compliance needs.</p>
+              <h2 className='price price-blue'>Custom Pricing</h2>
+              <p className='price price-blue'>All in one package</p>
+              <hr className='price-hr price-hr-blue' />
+              <div className="pricing-points blue-points">
+                <div className="point">
+                  <div className="tick"></div>
+                  <p>Custom instance sizing</p>
+                </div>
+                <div className="point">
+                  <div className="tick"></div>
+                  <p>Custom instance sizing</p>
+                </div>
+                <div className="point">
+                  <div className="tick"></div>
+                  <p>Custom instance sizing</p>
+                </div>
+                <div className="point">
+                  <div className="tick"></div>
+                  <p>Custom instance sizing</p>
+                </div>
+              </div>
+              <a href="" className='price-btn price-blue-btn'>Contact Us</a>
+            </div>
+          </div>
+          <div className="col-md-4 col-sm-12">
+            <div className="card">
+              <h2 className='card-type'>Enterprise</h2>
+              <p className='card-type-text'>For larger enterprises with support SLA and compliance needs.</p>
+              <h2 className='price'>Custom Pricing</h2>
+              <p className='price'>All in one package</p>
+              <hr className='price-hr' />
+              <div className="pricing-points">
+                <div className="point">
+                  <div className="tick"></div>
+                  <p>Custom instance sizing</p>
+                </div>
+                <div className="point">
+                  <div className="tick"></div>
+                  <p>Custom instance sizing</p>
+                </div>
+                <div className="point">
+                  <div className="tick"></div>
+                  <p>Custom instance sizing</p>
+                </div>
+                <div className="point">
+                  <div className="tick"></div>
+                  <p>Custom instance sizing</p>
+                </div>
+              </div>
+              <a href="" className='price-btn'>Contact Us</a>
+            </div>
+          </div>
+          <div className="col-md-4 col-sm-12">
+            <div className="card">
+              <h2 className='card-type'>Enterprise</h2>
+              <p className='card-type-text'>For larger enterprises with support SLA and compliance needs.</p>
+              <h2 className='price'>Custom Pricing</h2>
+              <p className='price'>All in one package</p>
+              <hr className='price-hr' />
+              <div className="pricing-points">
+                <div className="point">
+                  <div className="tick"></div>
+                  <p>Custom instance sizing</p>
+                </div>
+                <div className="point">
+                  <div className="tick"></div>
+                  <p>Custom instance sizing</p>
+                </div>
+                <div className="point">
+                  <div className="tick"></div>
+                  <p>Custom instance sizing</p>
+                </div>
+                <div className="point">
+                  <div className="tick"></div>
+                  <p>Custom instance sizing</p>
+                </div>
+              </div>
+              <a href="" className='price-btn'>Contact Us</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
