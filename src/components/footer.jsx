e08cb8074aded8a6df96a62427daf05a691c4d9e
ethan <ethanowicks9@gmail.com>
2024-02-10 18:54:04 +0000

    Style footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import { FaPhone } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
-import { FaInstagram } from "react-icons/fa";
-import { FaFacebook } from "react-icons/fa";
 
 
 
@@ -13,7 +11,7 @@ const Footer = () => {
         <div className="row">
             <div className="col-4 d-flex justify-content-center align-items-center flex-column my-5">
                 <div className="logo">
-                    <p>logo</p>
+                    <a className='footer-logo' href='/'>ElevateVisability</a>
                 </div>
                 <div className="phone mt-3">
                     <FaPhone />
@@ -23,43 +21,26 @@ const Footer = () => {
                     <MdEmail />
                     <a href="#">ethanowicks9@gmail.com</a>
                 </div>
-                <p className='my-4'>© 2024 ShapeHouse | FAQ Privacy Policy</p>
+                <div className="text-center">
+                    <p className='my-4 mx-3'>© 2024 ElevateVisability | FAQ Privacy Policy</p>
+                </div>
             </div>
             <div className="col-8 d-flex justify-content-center align-items-center">
                 <div className="footer-column">
                     <ul>
-                        <p>contact</p>
-                        <li>test</li>
-                        <li>test</li>
-                        <li>test</li>
-                        <li>test</li>
-                    </ul>
-                </div>
-                <div className="footer-column">
-                    <ul>
-                        <p>contact</p>
-                        <li>test</li>
-                        <li>test</li>
-                        <li>test</li>
-                        <li>test</li>
-                    </ul>
-                </div>
-                <div className="footer-column">
-                    <ul>
-                        <p>contact</p>
-                        <li>test</li>
-                        <li>test</li>
-                        <li>test</li>
-                        <li>test</li>
-                    </ul>
-                </div>
-                <div className="footer-column">
-                    <ul>
-                        <p>contact</p>
-                        <li>test</li>
-                        <li>test</li>
-                        <li>test</li>
-                        <li>test</li>
+                        <p className='links-title'>LINKS</p>
+                        <li>
+                            <a href="/pricing">Pricing</a>
+                        </li>
+                        <li>
+                            <a href="/about">About</a>
+                        </li>
+                        <li>
+                            <a href="/blos">Blogs</a>
+                        </li>
+                        <li>
+                            <a href="/Contact">Contact</a>
+                        </li>
                     </ul>
                 </div>
             </div>
