06f9ac551335fb2b4e0b39c5ed9b1dc30a266e5c
ethan <ethanowicks9@gmail.com>
2024-02-12 19:52:04 +0000

    Add text for blog posts

--- src/dummy.js
+++ src/dummy.js
@@ -29,26 +29,26 @@ export const blogData = () => {
     return [
       {
         "image": Blog1,
-        "title": "asdasd",
-        "author": "John Doe",
-        "text": "Refurbishing your bathroom can be an exciting project that can enhance the comfort, functionality and value of your home. However, before delving into the project, there are several important steps you should be taken to ensure that the renovation goes smoothly and meets your needs. Create an inventory of your current bathroom. What’s in you",
-        "date": "2024/02/01",
+        "title": "What is Web Development?",
+        "author": "David James",
+        "text": "Web development is the process of building websites and apps for the internet—or for a private network known as an intranet. Web developers bring the design and functionality of a website to life, writing code that determines things like style, layout, and interactivity. From the most simple, static web pages to social media platforms and apps; from e-commerce websites to content management systems (like WordPress)—all the tools we use via the internet have been built by web developers. There are three different types (or layers) of web development: The frontend, the backend, and database technology. Frontend development, otherwise known as client-side scripting, encompasses all the elements of a website that the user experiences directly. Things like layout, font, colors, menus, and contact forms are all powered by the frontend. Backend development, or server-side scripting, is all about what goes on behind the scenes. When you interact with a website in some way—for example, filling out a form and clicking “submit”—the frontend communicates this action to the backend. The backend responds by sending the relevant information to the frontend—for example, the code needed to display a message such as “Thank you for filling out this form”. The third layer comprises database technology. The database contains all the files and content that are necessary for a website to function, storing it in such a way that makes it easy to retrieve, organize, edit, and save. The frontend, backend, and database technology all work together to build and run a fully functional website or application. As such, these three layers form the foundation of web development. ",
+        "date": "03/01/2024",
         "id": 1
       },
       {
         "image": Blog2,
-        "title": "asdasd",
-        "author": "John Doe",
-        "text": "As renovation experts, we have found that many homeowners with traditional cottages aspire to modernise their living space by opening up the downstairs area. While this can certainly create a more spacious and contemporary feel, we often caution our clients against removing the downstairs WC. The downstairs WC may seem like an insignificant space, but ",
-        "date": "2024/02/01",
+        "title": "Why is web development important?",
+        "author": "John Fitzgerald Kennedy",
+        "text": "Can you believe that we're part of a world where over 5.30 billion people are connected through the Internet? That's more than half of our global population, actively engaging in research, connection, education, and entertainment through this incredible digital universe. Personally, I believe the Internet isn’t just a luxury; it's a fundamental part of our daily lives. Given the rapidly increasing number of Internet users, it’s no surprise that web development is a rapidly expanding industry. Between now and 2030, the employment of web developers is expected to grow by 13%, much faster than most other technology careers. It's an exciting time to be in this field, and I'm thrilled to be part of this dynamic industry.",
+        "date": "11/05/2023",
         "id": 2
       },
       {
         "image": Blog3,
-        "title": "asdasd",
-        "author": "John Doe",
-        "text": "As renovation experts, we have found that many homeowners with traditional cottages aspire to modernise their living space by opening up the downstairs area. While this can certainly create a more spacious and contemporary feel, we often caution our clients against removing the downstairs WC. The downstairs WC may seem like an insignificant space, but ",
-        "date": "2024/02/01",
+        "title": "9 Digital Healthcare UX Trends 2024",
+        "author": "graham wicks",
+        "text": "Healthcare UX Challenges. Ease of use. In the past few years, some relevant changes have occurred in healthcare. By now, not only medical professionals can take care of each patient’s health. Users also have the option to choose from a wide range of digital products. These products can assist them in improving performance and overall well-being. Treatments no longer remain boring must-haves; instead, they become services. Modern technology in the healthcare system provides patients with better service. It is also more comfortable and safer. But for this to work, the technology needs to be user-friendly. Aging population User-friendliness holds the key to the success of these apps and gadgets in the long run. Healthcare workers need to feel as if they have always used these tools. In our experience, a healthcare product’s design process must focus on what people want. We must consider what doctors struggle the most with and what treatments and tools the patients need. A good UX for healthcare apps should cater to a wide range of users. This includes those with visual, auditory, or cognitive impairments and limited internet access. This is crucial for healthcare tools, as it can benefit up to 25% of the population. Accessibility With the aging population, healthcare UI/UX designs should consider features for them. These include adjustable text size and contrast options. Video and audio alternatives, legible fonts and icons, and simple navigation are also worth mentioning. There is an increasing use of Electronic Health Records (EHR) in clinics and hospitals. Thus, protecting sensitive personal information is essential. Secure authorization measures are necessary to prevent data breaches and ensure patient privacy.",
+        "date": "22/04/2022",
         "id": 3
       },
       {
